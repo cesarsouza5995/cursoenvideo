85f48e9682dcca4f3ddb172513f7af2db18677e4
Cesar Souza <cesarsouza5995@gmail.com>
2020-04-04 14:31:09 -0300

    fazendo testes no git
    upando

--- Desafio016.py
+++ Desafio016.py
@@ -4,3 +4,4 @@ Desafio 16
 Crie um programa que leia um numero Real qualquer pelo teclado e mostre a sua porção inteira
 """
 import math
+inteiro = math.
---
+++ Desafio017.py
@@ -0,0 +1,5 @@
+"""
+Aula 008
+Desafio 17
+Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo retângulo, calcule e mostre o comprimento da hipotenusa.
+"""
